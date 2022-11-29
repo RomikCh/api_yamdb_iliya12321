240450b697561ab36b4bddd983b88f41661babd1
Орлов Сергей <orlovsergey263759223@gmail.com>
2022-11-29 19:06:37 +0300

    созданы новые модели

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -18,3 +18,71 @@ class User(AbstractUser):
         choices=ROLES,
         default='user'
     )
+
+
+class Category(models.Model):
+    name = models.CharField(
+        verbose_name='категория',
+        max_length=256,
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор категории',
+        max_length=50
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        verbose_name='жанр',
+        max_length=256,
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор жанра',
+        max_length=50,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=256,
+    )
+    year = models.IntegerField(
+        verbose_name='год издания'
+    )
+    description = models.TextField(
+        'описание',
+        null=True
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='titles'
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        verbose_name='Жанр',
+        related_name='titles',
+        blank=True,
+        through='GenreAndTitle',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class GenreAndTitle(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'жанр {self.title} - {self.genre}'
