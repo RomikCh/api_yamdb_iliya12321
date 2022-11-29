6dbd3bab97d130d09343848bf45e4fbfe44c95f4
Илья Воробьёв <iliya.vor2021@yandex.ru>
2022-11-29 19:58:36 +0300

    модель отзывов и коментов

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api.views import CommentViewSet, TitleViewSet
+
+
+router_v1 = DefaultRouter()
+
+router_v1.register(r'^titles/(?P<title_id>\d+)/reviews/(?P<rewiew_id>\d+/comments', CommentViewSet, basename='comments')
+router_v1.register(r'^titles/(?P<title_id>\d+)/reviews/', TitleViewSet, basename='titles')
+
+urlpatterns =  [
+    path('v1/', include(router_v1.urls)),
+]
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,9 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
 
+
+Title = 'Произведение'
 
 class User(AbstractUser):
     ROLES = (
@@ -18,3 +21,57 @@ class User(AbstractUser):
         choices=ROLES,
         default='user'
     )
+
+
+class Review(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews')
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews')
+    score = models.SmallIntegerField(
+        verbose_name="Оценка",
+        validators=[
+            MinValueValidator(
+                1,message='Убедитесь, что введено число больше или равно 1'
+            ),
+            MaxValueValidator(10, message='Убедитесь, что введено число меньше или равно 10')
+        ],
+    )
+    text = models.TextField("Текст", help_text="Отзыв")
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return '"{}" - отзыв на "{}" Автор: "{}"'.format(
+            self.text,
+            self.title,
+            self.author
+        )
+
+    class Meta:
+        ordering = ["-pub_date"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["author", "title"], name="unique_review"
+            )
+        ]
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField("Текст", help_text="Комментарий")
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return '"{}" - комментарий на данный отзыв: "{}" Автор: "{}"'.format(
+            self.text,
+            self.review,
+            self.author
+        )
+
+    class Meta:
+        ordering = ["-pub_date"]
