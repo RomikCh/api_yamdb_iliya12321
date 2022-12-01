eb87c45546be3ac0cff44976d7c8e8cbcc4d86b8
Anna Chendeva <110104564+Owlachno@users.noreply.github.com>
2022-12-01 20:04:28 +0300

    Update urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,7 +3,11 @@ from rest_framework.routers import DefaultRouter
 
 
 from api.views import (
-    CommentViewSet, TitleViewSet, CategoryViewSet, GenreViewSet,
+    CommentViewSet,
+    TitleViewSet,
+    CategoryViewSet,
+    GenreViewSet,
+    ReviewViewSet
 )
 
 app_name = 'api'
@@ -11,7 +15,7 @@ app_name = 'api'
 router_v1 = DefaultRouter()
 
 router_v1.register('titles', TitleViewSet)
-router_v1.register(r'^titles/(?P<title_id>\d+)/reviews/', TitleViewSet, basename='titles')
+router_v1.register(r'^titles/(?P<title_id>\d+)/reviews/', ReviewViewSet, basename='titles')
 router_v1.register(r'^titles/(?P<title_id>\d+)/reviews/(?P<rewiew_id>\d+/comments', CommentViewSet, basename='comments')
 router_v1.register('categories', CategoryViewSet)
 router_v1.register('genres', GenreViewSet)
