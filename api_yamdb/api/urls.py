83f8b6e40cb4217c284b7b62753678c37a488688
Anna Chendeva <110104564+Owlachno@users.noreply.github.com>
2022-12-01 20:03:31 +0300

    Update urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -10,11 +10,11 @@ app_name = 'api'
 
 router_v1 = DefaultRouter()
 
-router_v1.register(r'^titles/(?P<title_id>\d+)/reviews/(?P<rewiew_id>\d+/comments', CommentViewSet, basename='comments')
+router_v1.register('titles', TitleViewSet)
 router_v1.register(r'^titles/(?P<title_id>\d+)/reviews/', TitleViewSet, basename='titles')
+router_v1.register(r'^titles/(?P<title_id>\d+)/reviews/(?P<rewiew_id>\d+/comments', CommentViewSet, basename='comments')
 router_v1.register('categories', CategoryViewSet)
 router_v1.register('genres', GenreViewSet)
-router_v1.register('titles', TitleViewSet)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
