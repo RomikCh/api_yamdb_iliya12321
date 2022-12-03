9300cd01c57832ee141ca7903f511df2029a363b
Anna Chendeva <110104564+Owlachno@users.noreply.github.com>
2022-12-03 18:55:33 +0300

    Update permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -12,7 +12,7 @@ class IsAuthorModerAdminOrReadOnly(permissions.BasePermission):
         if request.method == 'POST':
             return request.user.is_authenticated
 
-        if request.method in ['PATCH', 'DELETE']:
+        if request.method in ['PUT', 'PATCH', 'DELETE']:
             return (
                 obj.author == request.user
                 or request.user.is_superuser
